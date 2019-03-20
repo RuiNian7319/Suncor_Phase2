4f5474fa25a4a75b064cf8869e2b808845744c5f
Rui Nian <35146558+RuiNian7319@users.noreply.github.com>
2019-03-19 20:33:29 -0600

    Update LinearRegressionObject.py

--- linear_regression_models/LinearRegressionObject.py
+++ linear_regression_models/LinearRegressionObject.py
@@ -390,7 +390,7 @@ def simulation(data_path, model_path, norm_path, test_size=0.05, shuffle=True, l
             # Global variables initializer
             sess.run(linear_reg.init)
 
-            for epoch in range(epochs):
+            for epoch in range(linear_reg.epochs):
 
                 for i in range(linear_reg.total_batch_number):
 
@@ -463,4 +463,6 @@ if __name__ == "__main__":
     Model_path = '/home/rui/Documents/Willowglen/Suncor_Phase2/linear_regression_models/checkpoints/ls.ckpt'
     Norm_path = '/home/rui/Documents/Willowglen/Suncor_Phase2/linear_regression_models/normalization/ls.pickle'
 
-    Raw_data, Heading_names, Linear_reg = simulation(Data_path, Model_path, Norm_path)
+    Raw_data, Heading_names, Linear_reg = simulation(Data_path, Model_path, Norm_path, test_size=0.05, shuffle=True, 
+                                                     lr=0.003, minibatch_size=2048, train_size=0.9, epochs=30, lambd=0.001, 
+                                                     testing=False)
